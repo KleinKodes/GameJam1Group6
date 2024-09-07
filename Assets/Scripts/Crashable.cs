6feaa131c4c09fbd889e1bc3e062c327f8e9aa1c
KleinKodes <kleinharrigan@gmail.com>
2024-09-07 13:21:05 -0400

    Added explosion vfx

--- Assets/Scripts/Crashable.cs
+++ Assets/Scripts/Crashable.cs
@@ -4,11 +4,15 @@ using UnityEngine;
 
 public class NewBehaviourScript : MonoBehaviour
 {
+    [SerializeField] private GameObject particleEffect;
     private void OnCollisionEnter(Collision collision)
     {
         if (collision.gameObject.CompareTag(Tags.DEATH))
         {
             //TODO: add call to player death function in player controller if it exists
+
+            //spawn explosion particle
+            Instantiate(particleEffect, gameObject.transform);
             GlobalEvents.PlayerDeath.invoke();
             
         }
